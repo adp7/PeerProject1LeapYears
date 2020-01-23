e2e0c62f1220c020587b6e03b6d9681d7328e6fe
adp7 <adpandey@pdx.edu>
2020-01-22 18:25:39 -0800

    Code modified

--- LeapYear/src/com/company/Main.java
+++ LeapYear/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int year = Integer.parseInt(args[0]);
+        if(year % 400 == 0){
+            System.out.println("It is a leap year!");
+        }
+        else if((year % 100 == 0) && (year % 400 != 0)){
+            System.out.println("It is a not leap year!");
+        }
+        else if((year % 4 == 0) && (year % 100 != 0)){
+            System.out.println("It is a leap year!");
+        }
+        else if((year % 4 != 0)){
+            System.out.println("It is not a leap year!");
+        }
     }
 }
