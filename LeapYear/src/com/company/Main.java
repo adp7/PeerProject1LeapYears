e09f1e1a1b75da6aef7bc66b67ab5ff5a1a053cb
adp7 <adpandey@pdx.edu>
2020-01-22 19:17:29 -0800

    Test Cases Added

--- LeapYear/src/com/company/Main.java
+++ LeapYear/src/com/company/Main.java
@@ -3,18 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int year = Integer.parseInt(args[0]);
-        if(year % 400 == 0){
-            System.out.println("It is a leap year!");
+        int year = 0;
+        if(args.length!=0) {
+            try {
+                year = Integer.parseInt(args[0]);
+            } catch (NumberFormatException nfe) {
+                System.err.println("Please enter a number!");
+            }
+
+            if (year < 0) {
+                System.err.println("Year shouldn't be negative");
+            }
+
+            LeapClass obj = new LeapClass();
+            if (obj.isLeapYear(year))
+                System.out.println("It is a leap year!");
+            else
+                System.out.println("It is not a leap year!");
         }
-        else if((year % 100 == 0) && (year % 400 != 0)){
-            System.out.println("It is a not leap year!");
-        }
-        else if((year % 4 == 0) && (year % 100 != 0)){
-            System.out.println("It is a leap year!");
-        }
-        else if((year % 4 != 0)){
-            System.out.println("It is not a leap year!");
-        }
+        else
+            System.err.println("Input is 0");
     }
 }
